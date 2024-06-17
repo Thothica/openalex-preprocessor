3a4e26414fe5cf8dd6a1fd995c32771f44cf5ee5
Tejas013 <tejasmadhukar6@gmail.com>
2024-06-17 14:45:49 +0530

    Crazy Optimisation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::Read;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
 
 use flate2::read::GzDecoder;
 use serde::{Deserialize, Serialize};
@@ -10,35 +14,76 @@ const OPENALEX_WORKS_DIRECTORY: &str = "openalex-snapshot-works/";
 #[derive(Deserialize, Debug, Serialize)]
 struct WorkObject {
     open_access: OpenAcess,
-    oa_url: Option<String>,
+    cited_by_count: u32,
+    title: String,
+    primary_topic: Topic,
+    publication_year: u32,
+    publication_date: Option<String>,
 }
 
 #[derive(Deserialize, Debug, Serialize)]
 struct OpenAcess {
     is_oa: bool,
+    oa_url: String,
+    oa_status: String,
 }
 
-// fn process_object() {}
+#[derive(Deserialize, Debug, Serialize)]
+struct Topic {
+    id: String,
+    domain: Domain,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct Domain {
+    display_name: String,
+}
+
+impl WorkObject {
+    fn is_useful(&self) -> bool {
+        if !self.open_access.is_oa && self.open_access.oa_status != "gold" {
+            return false;
+        }
+        if self.primary_topic.domain.display_name != "Social Sciences" {
+            return false;
+        }
+
+        true
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<flate2::read::GzDecoder<File>>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let gz = GzDecoder::new(file);
+    Ok(io::BufReader::new(gz).lines())
+}
 
 fn main() {
     // let mut handles = vec![];
 
+    let mut count = 0;
+
     for entry in WalkDir::new(OPENALEX_WORKS_DIRECTORY) {
         let path = entry.unwrap();
-        let bytes = match std::fs::read(path.path()) {
-            Ok(data) => data,
-            Err(_) => continue,
-        };
 
-        println!("{:?}", path.path());
+        if let Ok(contents) = read_lines(path.path()) {
+            for content in contents.flatten() {
+                let obj: WorkObject = match serde_json::from_str(&content) {
+                    Ok(data) => data,
+                    Err(_err) => {
+                        continue;
+                    }
+                };
 
-        let mut gz = GzDecoder::new(&bytes[..]);
-        let mut contents = String::new();
-        gz.read_to_string(&mut contents).unwrap();
-
-        for content in contents.lines() {
-            let obj: WorkObject = serde_json::from_str(content).unwrap();
-            println!("{}", obj.open_access.is_oa)
+                if obj.is_useful() {
+                    count += 1;
+                }
+            }
         }
     }
+
+    println!("Useful objects: {}", count)
 }
